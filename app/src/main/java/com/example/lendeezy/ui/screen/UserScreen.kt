b4f5c1fbb4da3865d8e53889b9652dc13734730e
Jeevan Ganatra <jeevanganatra@icloud.com>
2025-05-27 19:25:38 +0100

    Refactored User Screen into composables

--- app/src/main/java/com/example/lendeezy/ui/screen/UserScreen.kt
+++ app/src/main/java/com/example/lendeezy/ui/screen/UserScreen.kt
@@ -18,13 +18,14 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.example.lendeezy.data.model.User
 import com.example.lendeezy.ui.viewmodel.UserState
 import com.example.lendeezy.ui.viewmodel.UserViewModel
 
 @Composable
-fun UserScreen(viewModel: UserViewModel, padding: PaddingValues) {
+fun UserScreen(userViewModel: UserViewModel, padding: PaddingValues) {
 
-    val userState by viewModel.userState.collectAsState()
+    val userState by userViewModel.userState.collectAsState()
     val user = (userState as? UserState.Success)?.user
 
 
@@ -41,102 +42,121 @@ fun UserScreen(viewModel: UserViewModel, padding: PaddingValues) {
             .fillMaxWidth(),
     ) {
 
-        // === User Details ===
-        Box(
+        // user details section
+        if (user != null) {
+            UserDetailsSection(user)
+        }
+
+        // sign out button
+        SignOutSection(userViewModel)
+
+    }
+}
+
+/**
+ * user details box at top of screen
+ */
+@Composable
+fun UserDetailsSection(user: User) {
+    // === User Details ===
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFFF7F7F7))
+            .border(
+                width = 1.dp,
+                color = Color(0xFFD9D9D9),
+                shape = RoundedCornerShape(bottomStart = 24.dp, bottomEnd = 24.dp)
+            )
+    ) {
+
+        // Row for user details
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color(0xFFF7F7F7))
-                .border(
-                    width = 1.dp,
-                    color = Color(0xFFD9D9D9),
-                    shape = RoundedCornerShape(bottomStart = 24.dp, bottomEnd = 24.dp)
-                )
+                .padding(horizontal = 16.dp, vertical = 18.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
 
-            // Row for user details
-            Row(
+            // Profile image
+            AsyncImage(
+                model = user?.photoUrl,
+                contentDescription = "Profile Picture",
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 18.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .size(65.dp)
+                    .clip(CircleShape)
+            )
+
+            // spacer for between text and image
+            Spacer(modifier = Modifier.width(16.dp))
+
+            // Column for LHS with name and email
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.Center
             ) {
 
-                // Profile image
-                AsyncImage(
-                    model = user?.photoUrl,
-                    contentDescription = "Profile Picture",
-                    modifier = Modifier
-                        .size(65.dp)
-                        .clip(CircleShape)
+                Text(
+                    text = user.name ?: "User",
+                    style = MaterialTheme.typography.titleLarge
                 )
 
-                // spacer for between text and image
-                Spacer(modifier = Modifier.width(16.dp))
+                Spacer(modifier = Modifier.height(4.dp))
 
-                // Column for LHS with name and email
-                Column(
-                    modifier = Modifier.weight(1f),
-                    verticalArrangement = Arrangement.Center
-                ) {
-
-                    Text(
-                        text = user?.name ?: "User",
-                        style = MaterialTheme.typography.titleLarge
-                    )
-
-                    Spacer(modifier = Modifier.height(4.dp))
-
-                    Text(
-                        text = user?.email ?: "Email",
-                        style = MaterialTheme.typography.bodyLarge,
-                        color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f)
-                    )
-                }
-
+                Text(
+                    text = user.email ?: "Email",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f)
+                )
             }
+
         }
+    }
+}
 
 
-        // === Sign Out ===
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 12.dp)
-                .background(Color(0xFFF7F7F7))
-                .border(
-                    width = 1.dp,
-                    color = Color(0xFFD9D9D9),
-                )
+/**
+ * Sign out box with sign out button
+ */
+@Composable
+fun SignOutSection(userViewModel: UserViewModel) {
+    // === Sign Out ===
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp)
+            .background(Color(0xFFF7F7F7))
+            .border(
+                width = 1.dp,
+                color = Color(0xFFD9D9D9),
+            )
+    ) {
+
+        //row for sign out text on LHS and button on RHS
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp, vertical = 2.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
 
-            //row for sign out text on LHS and button on RHS
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp, vertical = 2.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+            // Sign out text
+            Text(
+                text = "Sign Out",
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+
+            // Sign out button calls function from UserViewModel
+            Button(
+                onClick = { userViewModel.signOut() },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black,
+                    contentColor = Color.White
+                ),
             ) {
-
-                // Sign out text
-                Text(
-                    text = "Sign Out",
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = MaterialTheme.colorScheme.onBackground
-                )
-
-                // Sign out button calls function from UserViewModel
-                Button(
-                    onClick = { viewModel.signOut() },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Black,
-                        contentColor = Color.White
-                    ),
-                ) {
-                    Text("Sign Out")
-                }
+                Text("Sign Out")
             }
         }
-
-
     }
 }
